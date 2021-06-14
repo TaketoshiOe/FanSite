3014732b23fd020550eb7aa7ab4407fd9beadebf
user <user@WIN-GVD5FNUS86K>
2021-06-14 16:28:20 +0900

    aa

--- FanSite/src/main/java/jp/co/taxis/fansite/FanSiteApplication.java
+++ FanSite/src/main/java/jp/co/taxis/fansite/FanSiteApplication.java
@@ -8,6 +8,7 @@ public class FanSiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FanSiteApplication.class, args);
+		
 	}
 
 }
--- FanSite/src/test/java/jp/co/taxis/funsite/FunsiteApplicationTests.java
+++ FanSite/src/test/java/jp/co/taxis/funsite/FunsiteApplicationTests.java
@@ -10,4 +10,5 @@ class FunsiteApplicationTests {
 	void contextLoads() {
 	}
 
+
 }
